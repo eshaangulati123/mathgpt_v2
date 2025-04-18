1f77db208c94812cd5cc1ca5cc27dcb6f91dcc30
eshaan gulati <eshaangulati123@gmail.com>
2025-04-18 18:24:27 -0400

    Remove scroll indicator arrow from hero section

--- src/App.tsx
+++ src/App.tsx
@@ -77,13 +77,6 @@ function Home() {
             </div>
           </div>
         </div>
-
-        {/* Subtle scroll indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-          <svg className="h-8 w-8 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-          </svg>
-        </div>
       </section>
 
       {/* Scrolling Logo Bar - right after hero */}
